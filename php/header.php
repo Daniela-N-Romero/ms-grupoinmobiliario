3ada549e5192e89ecbbf50a749fa8f4f3aaf21bc
Daniela Romero <danielanahirromero@gmail.com>
2024-06-05 22:49:40 -0300

    empece galeria

---
+++ php/header.php
@@ -0,0 +1,41 @@
+<header class="header">
+        
+        <nav class="container navbar navbar-expand-md d-md-flex" aria-label="Barra de navegacion">
+
+                <a href="/index.html" aria-label="Logo a página de inicio"><img src="./assets/logo-constructora.png" alt="Logo a página de inicio" class="logo" role="button" tabindex="0"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Botón de Barra de navegación">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto align-items-center">  
+
+                        <li class="nav-item"><a class="nav-link nav-link-block" aria-current="page" href="/index.html" role="button" tabindex="0" aria-label="Ir al inicio">Inicio</a></li>
+                        <li class="nav-item dropdown dropdown-header text-center"><a class="nav-link d-inline-block nav-link-inlineblock" href="./pages/nosotros.html" role="button" tabindex="0" aria-label="Informacion acerca de nosotros">Nosotros</a>
+                            <a class="nav-link dropdown-toggle d-inline-block" role="button"  tabindex="0" aria-label="Más informacion acerca de nosotros" data-bs-toggle="dropdown" aria-expanded="false"></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="/pages/nosotros.html"  role="button" tabindex="0" aria-label="Quienes somos">Quienes somos</a></li>
+                                <li><hr class="dropdown-divider"/></li>
+                                <li><a class="dropdown-item" href="/pages/nosotros.html"  role="button" tabindex="0" aria-label="Preguntas frecuentes">Preguntas frecuentes</a></li>
+                            </ul>
+                        </li>
+
+                                
+                            <li class="nav-item dropdown dropdown-header text-center"><a class="nav-link d-inline-block nav-link-inlineblock" href="./pages/obras.html" role="button"  tabindex="0" aria-label="Ver nuestras obras">Nuestras Obras</a>
+                                <a class="nav-link dropdown-toggle d-inline-block" role="button"  tabindex="0" aria-label="Ver más acerca de nuestras obras" data-bs-toggle="dropdown" aria-expanded="false"></a>
+                                <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="/pages/viviendas.html"  role="button" tabindex="0" alt="Ver viviendas personalizadas">Viviendas personalizadas</a></li>
+                                        <li><hr class="dropdown-divider"/></li>
+                                        <li><a class="dropdown-item" href="/pages/galerias.html"  role="button" tabindex="0" aria-label="Ver galerias y exteriores">Galerias y exteriores</a></li>
+                                        <li><hr class="dropdown-divider"/></li>
+                                        <li><a class="dropdown-item" href="/pages/modelos.html"  role="button" tabindex="0" aria-label="Ver modelos estándar">Modelos estándar</a></li>
+                                </ul>
+                            </li>
+                        
+                        <li class="nav-item"><a class="nav-link nav-link-block" href="/pages/contacto.html" role="button" tabindex="0" aria-label="Contactanos">Contactanos</a></li>
+                    </ul>
+                </div>
+
+        </nav>
+</header>
