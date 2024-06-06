f4e323ec43f9ecd61a1927a0b1df5f36561248b0
Daniela Romero <danielanahirromero@gmail.com>
2024-06-06 14:34:37 -0300

    falta enlazar botones de la galeria y revisar fotos

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,18 @@
 const body = document.body;
 let lastScroll = 0;
 
-window.addEventListener('scroll', ()=>{
+window.addEventListener("scroll", ()=>{
     const currentScroll = window.scrollY;
     if ( currentScroll <=0){
-        body.classList.remove('scroll-up')
+        body.classList.remove("scroll-up")
     }
-    if (currentScroll > lastScroll && !body.classList.contains('scroll-down')){
-        body.classList.remove('scroll-up');
-        body.classList.add('scroll-down');
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")){
+        body.classList.remove("scroll-up");
+        body.classList.add("scroll-down");
     }
-    if (currentScroll < lastScroll && body.classList.contains('scroll-down')){
-        body.classList.remove('scroll-down');
-        body.classList.add('scroll-up');
+    if (currentScroll < lastScroll && body.classList.contains("scroll-down")){
+        body.classList.remove("scroll-down");
+        body.classList.add("scroll-up");
     }
     lastScroll = currentScroll;
 });
@@ -54,26 +54,63 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-//carrousel
-debugger
+//carousel
 
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
-let carrouselDom = document.getElementsByClassName('carrousel');
-let carrouselList = document.getElementsByClassName('carrousel__list');
-let thumnailsDivDom = document.getElementsByClassName('thumbnails');
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+let carouselDom = document.getElementById("carousel");
+let carouselList = document.getElementById("carousel__list");
+let thumnailsDivDom = document.getElementById("thumbnails");
+
+let timeRunning = 3000;
+let runTimeOut;
+let timeAutoNext = 7000;
+
+let runAutoRun = setTimeout(()=>{
+  nextDom.click();
+},timeAutoNext);
+
 
 
 function showSlider(type){
-  let itemSlider = document.querySelectorAll('.carrousel__item')
-  let itemThumbnail = document.querySelectorAll('.thumbnail');
-  if (type ==='next'){
-    console.log(itemSlider)
-    // carrouselList.appendChild(itemSlider[0]);
-    // thumnailsDivDom.appendChild(itemThumbnail[0]);
-    // carrouselDom.classList.add("next");
+  let itemSlider = document.querySelectorAll(".carousel__item")
+  let itemThumbnail = document.querySelectorAll(".thumbnail");
+  if (type ==="next"){
+      carouselList.appendChild(itemSlider[0]);
+      thumnailsDivDom.appendChild(itemThumbnail[0]);
+      carouselDom.classList.add("next");
+  }else{
+      let positionLastItem = itemSlider.length - 1;
+      carouselList.prepend(itemSlider[positionLastItem]);
+      thumnailsDivDom.prepend(itemThumbnail[positionLastItem])
+      carouselDom.classList.add("prev");
   }
+
+
+
+      clearTimeout(runTimeOut);
+      runTimeOut = setTimeout(()=>{
+      carouselDom.classList.remove("next");
+      carouselDom.classList.remove("prev");
+      }, timeRunning);
+
+
+      clearTimeout(runAutoRun);
+      runAutoRun = setTimeout(()=>{
+        nextDom.click();
+      },timeAutoNext);
 
 }
 
-nextDom.addEventListener("click", showSlider('next'))
+//click next
+
+nextDom.addEventListener("click", function(type){
+  showSlider("next");
+})
+
+
+//click prev
+
+prevDom.addEventListener("click", function(type){
+  showSlider("prev");
+})
